0e3f78387ed02a26d546c6a05c7fed8293e23064
thanh hung nguyen <hunnguye@galatee.inria.fr>
2015-07-31 19:09:20 +0200

    add subset 2

--- PA2/Subset.java
+++ PA2/Subset.java
@@ -1,6 +1,20 @@
+
+
 public class Subset {
    public static void main(String[] args)
    {
-       int N = Integer.parseInt(args[0]);
+       RandomizedQueue<String> q = new RandomizedQueue<>();
+       int k = Integer.parseInt(args[0]);
+       while (!StdIn.isEmpty() && k > 0) {
+           if(StdRandom.uniform(0,2)==1)
+           {
+               q.enqueue(StdIn.readString());
+           }
+       }
+       
+       while(!q.isEmpty())
+       {
+           StdOut.println(q.dequeue());
+       }
    }
 }
